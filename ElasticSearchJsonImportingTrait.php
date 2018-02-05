51ec1b77b09c2295001553739c654f0fdfb91468
Andy Truong <thehongtt@gmail.com>
2018-02-05 11:00:34 +0700

    Improve ElasticSearchJsonImportingTrait.php

--- ElasticSearchJsonImportingTrait.php
+++ ElasticSearchJsonImportingTrait.php
@@ -3,24 +3,100 @@
 namespace go1\util_dataset;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Elasticsearch\Client;
 use go1\util\edge\EdgeTypes;
 use go1\util\enrolment\EnrolmentStatuses;
 use go1\util\es\Schema;
 use go1\util\schema\mock\EnrolmentMockTrait;
+use go1\util\schema\mock\LoMockTrait;
 use go1\util\schema\mock\UserMockTrait;
 use ONGR\ElasticsearchDSL\Query\TermLevel\TermQuery;
 use ONGR\ElasticsearchDSL\Search;
+use stdClass;
 
 trait ElasticSearchJsonImporting
 {
-    protected function import(Client $es, $portalIndex, string $jsonDirectory)
+    protected function importMapping(string $jsonDirectory)
     {
-        $_ = [
+        return [
+            # Order is important
             Schema::O_ENROLMENT => $jsonDirectory . '/enrolments.json',
             Schema::O_ACCOUNT   => $jsonDirectory . '/accounts.json',
             Schema::O_LO        => $jsonDirectory . '/learning-objects.json',
         ];
+    }
+
+    protected function import(Connection $go1, Client $es, string $portalIndex, string $accountsName, string $jsonDirectory)
+    {
+        foreach (static::importMapping($jsonDirectory) as $type => $file) {
+            $objects = json_decode(file_get_contents($file));
+
+            foreach ($objects as $object) {
+                $es->index([
+                    'index'   => $portalIndex,
+                    'routing' => $portalIndex,
+                    'type'    => $type,
+                    'id'      => $object->id,
+                    'body'    => $object,
+                    'refresh' => true,
+                ]);
+
+                switch ($type) {
+                    case Schema::O_ENROLMENT:
+                        $this->importEnrolment($go1, $es, $portalIndex, $object);
+                        break;
+
+                    case Schema::O_ACCOUNT:
+                        $this->importAccount($go1, $es, $portalIndex, $object, $accountsName);
+                        break;
+
+                    case Schema::O_LO:
+                        $this->importLearningObject($go1, $es, $portalIndex, $object);
+
+                        break;
+                }
+            }
+        }
+    }
+
+    protected function importEnrolment(Connection $go1, Client $es, string $portalIndex, $object)
+    {
+        $api = new class
+        {
+            use EnrolmentMockTrait;
+            use UserMockTrait;
+        };
+
+        $go1->update('gc_user', ['profile_id' => $object->profile_id], ['id' => $object->metadata->user_id]);
+
+        $enrolmentId = $api->createEnrolment($go1, [
+            'id'                => $object->id,
+            'lo_id'             => $object->lo->id,
+            'profile_id'        => $object->profile_id,
+            'status'            => EnrolmentStatuses::toString($object->status),
+            'taken_instance_id' => $object->metadata->instance_id,
+        ]);
+
+        if (!empty($object->assessors)) {
+            foreach ($object->assessors as $assessorId) {
+                $api->link(
+                    $go1,
+                    EdgeTypes::HAS_TUTOR_ENROLMENT_EDGE,
+                    $assessorId,
+                    $enrolmentId
+                );
+            }
+        }
+    }
+
+    protected function importAccount(Connection $go1, Client $es, string $portalIndex, stdClass $object, string $accountsName)
+    {
+        $api = new class
+        {
+            use EnrolmentMockTrait;
+            use UserMockTrait;
+        };
 
         $findUserId = function ($mail) use ($es, $portalIndex) {
             $results = $es->search([
@@ -36,95 +112,50 @@ trait ElasticSearchJsonImporting
             return $results['hits']['hits'][0]['_source']['metadata']['user_id'] ?? null;
         };
 
-        $mock = new class
-        {
-            use EnrolmentMockTrait;
-            use UserMockTrait;
-        };
-
-        foreach ($_ as $type => $file) {
-            $objects = json_decode(file_get_contents($file));
-
-            foreach ($objects as $object) {
-                $es->index([
-                    'index'   => $portalIndex,
-                    'routing' => $portalIndex,
-                    'type'    => $type,
-                    'id'      => $object->id,
-                    'body'    => $object,
-                    'refresh' => true,
-                ]);
-
-                switch ($type) {
-                    case Schema::O_ACCOUNT:
-                        try {
-                            $this->link(
-                                $go1,
-                                EdgeTypes::HAS_ACCOUNT,
-                                $this->createUser($go1, [
-                                    'id'         => $findUserId($object->mail),
-                                    'instance'   => $accountsName,
-                                    'mail'       => $object->mail,
-                                    'first_name' => $object->first_name,
-                                    'last_name'  => $object->last_name,
-                                ]),
-                                $this->createUser($go1, [
-                                    'id'         => $object->id,
-                                    'instance'   => $object->instance,
-                                    'mail'       => $object->mail,
-                                    'first_name' => $object->first_name,
-                                    'last_name'  => $object->last_name,
-                                    'profile_id' => $object->profile_id,
-                                ])
-                            );
-                        }
-                        catch (UniqueConstraintViolationException $e) {
-                        }
-                        break;
-
-                    case Schema::O_LO:
-                        $loId = $this->createLO($go1, [
-                            'id'          => $object->id,
-                            'title'       => $object->title,
-                            'type'        => $object->type,
-                            'instance_id' => $object->instance_id,
-                        ]);
-
-                        if (!empty($object->authors)) {
-                            foreach ($object->authors as $author) {
-                                $this->link($go1, EdgeTypes::HAS_AUTHOR_EDGE, $loId, $author->id);
-                            }
-                        }
-
-                        break;
-
-                    case Schema::O_ENROLMENT:
-                        break;
-                }
-            }
+        try {
+            $api->link(
+                $go1,
+                EdgeTypes::HAS_ACCOUNT,
+                $api->createUser($go1, [
+                    'id'         => $findUserId($object->mail),
+                    'instance'   => $accountsName,
+                    'mail'       => $object->mail,
+                    'first_name' => $object->first_name,
+                    'last_name'  => $object->last_name,
+                ]),
+                $api->createUser($go1, [
+                    'id'         => $object->id,
+                    'instance'   => $object->instance,
+                    'mail'       => $object->mail,
+                    'first_name' => $object->first_name,
+                    'last_name'  => $object->last_name,
+                    'profile_id' => $object->profile_id,
+                ])
+            );
+        }
+        catch (UniqueConstraintViolationException $e) {
         }
     }
 
-    protected function importEnrolment(Connection $go1, $object)
+    protected function importLearningObject(Connection $go1, Client $es, string $portalIndex, $object)
     {
-        $go1->update('gc_user', ['profile_id' => $object->profile_id], ['id' => $object->metadata->user_id]);
+        $api = new class
+        {
+            use EnrolmentMockTrait;
+            use LoMockTrait;
+            use UserMockTrait;
+        };
 
-        $enrolmentId = $mock->createEnrolment($go1, [
-            'id'                => $object->id,
-            'lo_id'             => $object->lo->id,
-            'profile_id'        => $object->profile_id,
-            'status'            => EnrolmentStatuses::toString($object->status),
-            'taken_instance_id' => $object->metadata->instance_id,
+        $loId = $api->createLO($go1, [
+            'id'          => $object->id,
+            'title'       => $object->title,
+            'type'        => $object->type,
+            'instance_id' => $object->instance_id,
         ]);
 
-        if (!empty($object->assessors)) {
-            foreach ($object->assessors as $assessorId) {
-                $mock->link(
-                    $go1,
-                    EdgeTypes::HAS_TUTOR_ENROLMENT_EDGE,
-                    $assessorId,
-                    $enrolmentId
-                );
+        if (!empty($object->authors)) {
+            foreach ($object->authors as $author) {
+                $api->link($go1, EdgeTypes::HAS_AUTHOR_EDGE, $loId, $author->id);
             }
         }
     }

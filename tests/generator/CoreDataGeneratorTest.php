a5786c4a736f0fd2bcb9c9f0b585f78d195af67b
Andy Truong <thehongtt@gmail.com>
2018-04-09 18:00:05 +0700

    Add WIP.

---
+++ tests/generator/CoreDataGeneratorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace go1\util_dataset\tests\generator;
+
+use PHPUnit\Framework\TestCase;
+
+class CoreDataGeneratorTest extends TestCase
+{
+    public function test()
+    {
+        $this->markTestIncomplete('WIP');
+    }
+}

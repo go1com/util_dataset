35e23ad292e1a93e10a22e47a5a05b85199b4e35
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-08-13 21:12:37 +0700

    GO1P-20407 Add learning items to modules

--- generator/core/LoDataGenerator.php
+++ generator/core/LoDataGenerator.php
@@ -16,7 +16,16 @@ class LoDataGenerator
     #       2. Web for everyone meet-up.
     #   Modules:
     #       1. Basics of HTML
+    #           1. Introduction to Tags
+    #           2. Introduction to Elements
+    #           3. Introduction to Attributes
     #       2. Introduction to CSS
+    #           1. Selectors and Visual Rules
+    #           2. The Box Model
+    #           3. Display and Positioning
+    #           4. Colors
+    #           5. Typography
+    #           6. Grid
     # ---------------------
     public $courseWebId;
     public $courseWebTitle                           = 'Making web 101';
@@ -37,6 +46,24 @@ class LoDataGenerator
     public $moduleHtmlTitle                          = 'Basics of HTML';
     public $moduleCssId;
     public $moduleCssTitle                           = 'Introduction to CSS';
+    public $liIntroductionToTagsId;
+    public $liIntroductionToElementsId;
+    public $liIntroductionToAttributesId;
+    public $liSelectorsAndVisualRulesId;
+    public $liTheBoxModelId;
+    public $liDisplayAndPositioningId;
+    public $liColorsId;
+    public $liTypographyId;
+    public $liGridId;
+    public $liIntroductionToTagsTitle                 = 'Introduction to Tags';
+    public $liIntroductionToElementsTitle             = 'Introduction to Elements';
+    public $liIntroductionToAttributesTitle           = 'Introduction to Attributes';
+    public $liSelectorsAndVisualRulesTitle            = 'Selectors and Visual Rules';
+    public $liTheBoxModelTitle                        = 'The Box Model';
+    public $liDisplayAndPositioningTitle              = 'Display and Positioning';
+    public $liColorsTitle                             = 'Colors';
+    public $liTypographyTitle                         = 'Typography';
+    public $liGridTitle                               = 'Grid';
 
     /**
      * @param CoreDataGeneratorTrait $trait
@@ -93,9 +120,70 @@ class LoDataGenerator
             'seats' => $trait->eventWeb4EveryOneAvailableSeats,
         ]);
 
+        $trait->liIntroductionToTagsId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::ACTIVITY,
+            'title'       => $trait->liIntroductionToTagsTitle,
+        ]);
+
+        $trait->liIntroductionToElementsId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::ATTENDANCE,
+            'title'       => $trait->liIntroductionToElementsTitle,
+        ]);
+
+        $trait->liIntroductionToAttributesId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::ASSIGNMENT,
+            'title'       => $trait->liIntroductionToAttributesTitle,
+        ]);
+
+        $trait->liSelectorsAndVisualRulesId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::DOCUMENT,
+            'title'       => $trait->liSelectorsAndVisualRulesTitle,
+        ]);
+
+        $trait->liTheBoxModelId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::IFRAME,
+            'title'       => $trait->liTheBoxModelTitle,
+        ]);
+
+        $trait->liDisplayAndPositioningId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::INTERACTIVE,
+            'title'       => $trait->liDisplayAndPositioningTitle,
+        ]);
+
+        $trait->liColorsId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::QUESTION,
+            'title'       => $trait->liColorsTitle,
+        ]);
+
+        $trait->liTypographyId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::QUIZ,
+            'title'       => $trait->liTypographyTitle,
+        ]);
+
+        $trait->liGridId = $api->createLO($trait->go1, [
+            'instance_id' => $trait->portalId,
+            'type'        => LiTypes::RESOURCE,
+            'title'       => $trait->liGridTitle,
+        ]);
+
         $api->link($trait->go1, EdgeTypes::HAS_MODULE, $trait->courseWebId, $trait->moduleHtmlId);
         $api->link($trait->go1, EdgeTypes::HAS_MODULE, $trait->courseWebId, $trait->moduleCssId);
-        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->courseWebId, $trait->eventWeb4EveryOneLiId);
-        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->courseWebId, $trait->eventUnderstandWebIn4HoursLiId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleHtmlId, $trait->liIntroductionToTagsId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleHtmlId, $trait->liIntroductionToElementsId);
+        $api->link($trait->go1, EdgeTypes::HAS_ELECTIVE_LI, $trait->moduleHtmlId, $trait->liIntroductionToAttributesId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleCssId, $trait->liSelectorsAndVisualRulesId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleCssId, $trait->liTheBoxModelId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleCssId, $trait->liDisplayAndPositioningId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleCssId, $trait->liColorsId);
+        $api->link($trait->go1, EdgeTypes::HAS_LI, $trait->moduleCssId, $trait->liTypographyId);
+        $api->link($trait->go1, EdgeTypes::HAS_ELECTIVE_LI, $trait->moduleCssId, $trait->liGridId);
     }
 }
